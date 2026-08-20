28927c6d625b594f85123ea3f4dbbe8a5a04bc86
Hzak <nurmawatypotale@gmail.com>
2026-08-20 18:37:51 +0800

    Update FallForBrainrot.lua

--- Script/FallForBrainrot.lua
+++ Script/FallForBrainrot.lua
@@ -1,1 +1,78 @@
+local _version = "1.6.66"
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/download/" .. _version .. "/main.lua"))()
 
+local Window = WindUI:CreateWindow({
+    Title = "NeuroFall", -- window title
+    Icon = "https://raw.githubusercontent.com/KHAFIDZ657/NeuroHub/main/Image/MainIcon.png", -- lucide icon or "rbxassetid://" or URL. optional
+    Author = "by Khafidz.", -- window subtitle. optional
+    Folder = "NeuroFall", -- folder to save keys and images
+    
+    User = { -- user information located at the bottom left
+        Enabled = true, -- can be toggled with Window.User:Enable() or Window.User:Disable()
+        Anonymous = false, -- can be toggled with Window.User:SetAnonymous(true) --(true or false)
+        Callback = function() -- callback on click. optional. it can be removed
+            print("Just Callback of User button. ")
+        end,
+    },
+})
+
+local About = Window:Tab({
+    Title = "About",
+    Desc = "About NeuroHub", -- optional
+    Icon = "badge-info", -- lucide icon or "rbxassetid://" or URL. optional
+    IconColor = Color3.fromRGB(173, 216, 230), -- custom icon color. optional
+    IconShape = "Square", -- "Square" or "Circle". optional
+    IconThemed = true, -- use theme colors. optional
+    Locked = false, -- disable tab interaction. optional
+    ShowTabTitle = false, -- show title inside tab. optional
+    Border = true, -- add border around tab. optional
+    CustomEmptyPage = { -- custom empty page when no elements are added to the tab. optional
+		Icon = "lucide:smile", -- icon for empty page. optional
+		Title = "empty", -- title for empty page. optional
+		Desc = "Just a tabs normal", -- description for empty page. optional
+	},
+})
+
+local Version = Window:Tag({
+    Title = "1.1",
+    Icon = "rocket", -- optional
+    Color = Color3.fromRGB(0, 0, 128), -- custom color
+})
+
+local Paragraph = About:Paragraph({
+    Title = "About",
+    Desc = "NeuroHub is a script designed to provide basic yet useful features.\nVersion 1.0 "
+})
+
+local Farm = Window:Tab({
+    Title = "Farm",
+    Desc = "Auto Farming", -- optional
+    Icon = "repeat", -- lucide icon or "rbxassetid://" or URL. optional
+    IconColor = Color3.fromRGB(255, 100, 100), -- custom icon color. optional
+    IconShape = "Square", -- "Square" or "Circle". optional
+    IconThemed = true, -- use theme colors. optional
+    Locked = false, -- disable tab interaction. optional
+    ShowTabTitle = false, -- show title inside tab. optional
+    Border = true, -- add border around tab. optional
+    CustomEmptyPage = { -- custom empty page when no elements are added to the tab. optional
+		Icon = "lucide:smile", -- icon for empty page. optional
+		Title = "Empty", -- title for empty page. optional
+		Desc = "Maybe In Development", -- description for empty page. optional
+	},
+})
+
+local Scan = Farm:Button({
+    Title = "Scan",
+    Desc = "Scan Folder", -- optional
+    Icon = "mouse-pointer-click", -- lucide icon or "rbxassetid://". optional
+    IconAlign = "Right", -- "Left" or "Right". optional
+    IconColor = Color3,fromHex("#ffffff"), -- custom icon color. optional
+    Color = Color3.fromRGB(100, 100, 255), -- custom color. optional
+    Locked = false, -- disable button. optional
+    LockedTitle = "Locked", -- text shown when locked. optional
+    Justify = "Between", -- "Between" or "Center". optional
+    
+    Callback = function()
+        print("Clicked!")
+    end
+})
